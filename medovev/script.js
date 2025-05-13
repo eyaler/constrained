7cbeef4de2d51eff7ab7c8175f2d3ce1d713a73f
Eyal Gruss <eyalgruss@gmail.com>
2025-05-14 01:30:05 +0300

    medovev perfect

--- medovev/script.js
+++ medovev/script.js
@@ -3,12 +3,17 @@
 // https://phuoc.ng/collection/mirror-a-text-area/
 
 
-function process(text) {
+function process(text, perfect) {
   let normalized = ''
   const positions = {}
+  if (perfect)
+    text = text.replace(/[\s-\u05be\u2013\u2014]/g, ' ')
   for (let i = 0; i < text.length; i++) {
     const char = text[i]
-    if (char.match(/[\p{L}\p{N}]/u)) {
+    while (perfect && char[0] == ' ' && text[i + 1] == ' ') {
+      i++
+    }
+    if (char.match(/[\p{L}\p{N}]/u) || perfect && char[0] == ' ') {
       normalized += char.toLowerCase().normalize('NFKD').replace(/\p{M}/gu, '').replace(/ך/, 'כ').replace(/ם/, 'מ').replace(/ן/, 'נ').replace(/ף/, 'פ').replace(/ץ/, 'צ')
       positions[normalized.length - 1] = i
     }
@@ -32,7 +37,7 @@ function check_outer(text) {
       return [n - 1 - i, i]
 }
 
-const abc = 'abcdefghijklm' + 'אבגדהוזחטיכלמנסעפצקרשת' + 'nopqrstuvwxyz'
+const abc = 'abcdefghijklm' + 'אבגדהוזחטיכ  למנסעפצקרשת' + 'nopqrstuvwxyz'
 const comp = Object.fromEntries([...abc].map((c, i) => [c, abc[abc.length - 1 - i]]))
 
 function check_bio(text) {
@@ -67,7 +72,7 @@ function update(text) {
     if (bio)
       is_palindrome = 'ביו־' + is_palindrome
     else {
-      const inner = check_inner(normalized)   
+      const inner = check_inner(normalized)
       if (inner) {
         if (inner[0] == mid)
           marks = marks.map(([pos, cls]) => [pos, cls + ' inner'])
@@ -78,6 +83,11 @@ function update(text) {
           marks = [[outer[0], 'outer'], ...marks, [outer[1], 'outer']]
         is_palindrome = ''      
       }
+    }
+    if (is_palindrome) {
+        const [normalized, positions] = process(text, true)
+        if (!check_inner(normalized) || check_bio(normalized))
+            is_palindrome += ' מושלם'
     }
   }
   marks.forEach(([pos, cls]) => text = text.slice(0, positions[pos]) + `<mark class="${cls}">${text[positions[pos]]}</mark>` + text.slice(positions[pos] + 1))
